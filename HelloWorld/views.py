1231000074653b2058999aa6913a2a22125f737b
AVIRAL GUPTA <21f1000608@student.onlinedegree.iitm.ac.in>
2022-01-12 00:46:25 +0530

    done

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -6,11 +6,7 @@ def index(request):
     developed_by = "Aviral"
     languages = [
         "JAVA",
-<<<<<<< HEAD
         "C++",
-=======
-        "C++"",
->>>>>>> d437a416e87faa2f00de9244185251857d7399fa
         "HTML",
         "CSS",
         "JAVASCRIPT",
@@ -18,13 +14,8 @@ def index(request):
         ]
 
     context = {
-<<<<<<< HEAD
-            "developer": developed_by,
-            "languages": languages
-=======
             developer: "developed_by",
             languages: "languages"
->>>>>>> d437a416e87faa2f00de9244185251857d7399fa
         }
     response = render(request, 'index.html', context)
 
